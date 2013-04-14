f29544390785bc7d27da38ec11f199756a1e90fb
Live session user <ubuntu@ubuntu.(none)>
2013-04-14 11:49:09 +0200

    start app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookshelf::Application.config.secret_token = 'da359ca2cd1f4de4e32de862b071002091f1b4d59e1ce65ae1369137852273f258df5b1453d5a01f54081a0df848e281baa9773acb7809845e49d0af23f36e55'
